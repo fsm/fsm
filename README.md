3cd270b5d3c083f949f1e532cb367b9ea5e17a9b
Brandon Romano <BrandonRRomano@gmail.com>
2018-01-25 11:39:03 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# FSM
+
+This package contains a simple interface for a [finite-state machine](https://en.wikipedia.org/wiki/Finite-state_machine) in Go.
+
+# License
+
+[MIT](LICENSE.md)
---
+++ fsm.go
@@ -0,0 +1,42 @@
+package fsm
+
+// StateMachine is a k:v map that maps the
+// slug of States to BuildState functions
+type StateMachine map[string]BuildState
+
+// BuildState is a function that generates a State
+// with access to a specific Emitter and Traverser
+type BuildState func(Emitter, Traverser) *State
+
+// State represents an individual state in a larger state machine
+type State struct {
+	Slug        string
+	EntryAction func() error
+	Transition  func(interface{}) *State
+}
+
+// Emitter is a generic interface to output arbitrary data.
+// Emit is generally called from State.EntryAction.
+type Emitter interface {
+	Emit(interface{}) error
+}
+
+// A Store is a generic interface responsible for managing
+// The fetching and creation of traversers
+type Store interface {
+	FetchTraverser(uuid string) (Traverser, error)
+	CreateTraverser(uuid string) (Traverser, error)
+}
+
+// A Traverser is an individual that is traversing the
+// StateMachine.  This interface that is responsible
+// for managing the state of that individual
+type Traverser interface {
+	UUID() string
+	SetUUID(string)
+	CurrentState() string
+	SetCurrentState(string)
+	Upsert(key string, value interface{}) error
+	Fetch(key string) (interface{}, error)
+	Delete(key string) error
+}
